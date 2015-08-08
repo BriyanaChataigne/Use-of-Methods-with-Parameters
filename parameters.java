caafb6df1c07fef79cf36b513ef40b53b776ebe6
BriyanaChataigne <briyana.chataigne@techhigh.us>
2015-08-07 20:34:11 -0400

    added parameters.java

---
+++ parameters.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+ public class apples {
+		public static void main (String args[]){
+			Scanner input = new Scanner(System.in);
+		nutella bread = new nutella();
+		System.out.println("Enter your name here: ");
+		String name = input.nextLine();
+		bread.message(name);
+		}
+	}
